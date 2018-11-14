fd8039d31a0d9cd4ebe697fdb785b1103913f7bc
Simmons, Spence D <130499@s020rmc148-04.local>
2018-11-14 09:34:38 -0600

    2nd branch

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Simmons, Spence D on 11/14/18.
+//  Copyright © 2018 Simmons, Spence D. All rights reserved.
+//
+
+import Foundation
--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -19,6 +19,11 @@ class ViewController: UIViewController {
         print("This is a Git tutorial")
     }
 
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
